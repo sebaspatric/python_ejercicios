4b516c3a5b1f3bd5cd12fcbca4be8ddea58979dc
¨sebaspatric¨ <sebastian.menares@gmail.com>
2023-04-16 00:11:54 -0400

    4 commit

---
+++ ejercicio3/ejercicio4.py
@@ -0,0 +1,12 @@
+#>mostrar los numeros del 1 al 100 en orden inverso
+
+
+"""
+for i in range(1,101):
+    print(i)
+"""
+numero = 100
+for i in range(0,100):
+    ##print(i,end=" ")       
+    print(numero,end=" ")
+    numero = numero - 1 
